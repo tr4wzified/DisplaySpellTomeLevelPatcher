eb2415a988e2267e980564f526814e95bb1a5977
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-04-27 20:47:24 +0200

    Restore default

--- DisplaySpellTomeLevelPatcher/Settings.cs
+++ DisplaySpellTomeLevelPatcher/Settings.cs
@@ -10,6 +10,6 @@ namespace DisplaySpellTomeLevelPatcher
     public class Settings
     {
         [SynthesisTooltip(@"Specify your own format here! Available variables: <level> (ex. Adept), <spell> (ex. Clairvoyance), <plugin> (ex. Skyrim), <school> (ex. Alteration). Default: Spell Tome (<level>): <spell>")]
-        public string Format { get; set; } = "Spell Tome (<level>): <spell> from <plugin> with school <school>";
+        public string Format { get; set; } = "Spell Tome (<level>): <spell>";
     }
 }
