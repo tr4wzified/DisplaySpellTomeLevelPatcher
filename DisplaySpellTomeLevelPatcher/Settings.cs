d2a513366f9096b7e841b778d1f21e542289f5a9
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-04-27 13:58:06 +0200

    Add tooltip

--- DisplaySpellTomeLevelPatcher/Settings.cs
+++ DisplaySpellTomeLevelPatcher/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mutagen.Bethesda.Synthesis.Settings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace DisplaySpellTomeLevelPatcher
 {
     public class Settings
     {
+        [SynthesisTooltip("If checked, naming will be <skill level> Spell Tome: <spell> instead of Spell Tome (<skill level>): <spell>.")]
         public bool AlternativeNaming { get; set; } = false;
     }
 }
