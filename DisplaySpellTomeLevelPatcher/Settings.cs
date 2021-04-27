9907cf4f64d34cbc7ffb0cf4920c583495b7a275
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-04-27 20:31:33 +0200

    Remove Alternative Naming var, replaced by Format

--- DisplaySpellTomeLevelPatcher/Settings.cs
+++ DisplaySpellTomeLevelPatcher/Settings.cs
@@ -9,8 +9,6 @@ namespace DisplaySpellTomeLevelPatcher
 {
     public class Settings
     {
-        [SynthesisTooltip(@"If checked, naming will be '<skill level> Spell Tome: <spell>' instead of 'Spell Tome (<skill level>): <spell>'.")]
-        public bool AlternativeNaming { get; set; } = false;
         [SynthesisTooltip(@"Specify your own format here! Available variables: <level> (ex. Adept), <spell> (ex. Clairvoyance), <plugin> (ex. Skyrim), <school> (ex. Alteration). Default: Spell Tome (<level>): <spell>")]
         public string Format { get; set; } = "Spell Tome (<level>): <spell> from <plugin> with school <school>";
     }
